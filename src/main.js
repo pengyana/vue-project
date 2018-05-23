750c2836cd21f25310eff3c1477a636273c2d7b2
pengyan_ <1291551591@qq.com>
2018-05-23 16:42:18 +0800

    新增文件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
+import layer from 'vue-layer'
+Vue.prototype.$layer = layer(Vue);
+//公用css样式
+import '@/assets/css/lib-reset.css'
+import '@/assets/css/lib-util.css'
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import IndexList from '@/page/index-list'
+import leftMenu from '@/components/leftMenu'
+
+
 
 Vue.use(Router)
 
@@ -8,8 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'IndexList',
+      component:IndexList
+    },
+    {
+      path: '/leftMenu',
+      name: 'leftMenu',
+      component:leftMenu
     }
   ]
 })

9eabcabf2998d7fbca35a5c5f07c9c14bc2919da
pengyan_ <1291551591@qq.com>
2018-05-23 16:51:51 +0800

    新增文件

---
+++ src/components/leftMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="leftMenu">
+    <ul class="leftMenu-ul fix">
+      <li class="leftMenu-li">
+        <a class="leftMenu-a"></a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    methods : {
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../../static/css/index-list.css';
+</style>

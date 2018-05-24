e22cb7ded233b2055f2d9c5b474a4f23f6f188c7
pengyan_ <1291551591@qq.com>
2018-05-24 16:51:59 +0800

    增加组件

---
+++ src/components/inputView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="info-body content f12">
+    <div v-for="mesgData in listData">
+      <div v-if="mesgData.inputType" class="form-group form-mt">
+        <div class="form-label form-width">{{mesgData.textTitle}}：</div>
+        <input class="form-control"
+          @blur="blurFun"
+          :placeholder="mesgData.inputAttr.placeholder"
+          :class="mesgData.inputAttr.inputClasses"
+          :name="mesgData.inputAttr.name"
+          :readonly="mesgData.inputAttr.readonly"
+          :type="mesgData.inputAttr.type"
+          :disabled="mesgData.inputAttr.disabled"
+          :style="{width: mesgData.inputAttr.widthChange}"
+          v-model="mesgData.inputAttr.val"
+        />
+      </div>
+      <div v-if="mesgData.listType" class="form-group form-mt">
+          <label class="form-label form-width">{{mesgData.textTitle}}：</label>
+          <div class="form-control-text formR-text tl">
+            {{mesgData.inputAttr.name}}
+          </div>
+      </div>
+
+    </div>
+
+    <!--<input type="text" placeholder="请输入年龄" name="age" v-model="registerData.age"/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      registerData:this.listData,
+    }
+  },
+  props:['listData'],
+  methods:{
+    blurFun(){
+      console.log(this.registerData)
+//      console.log(this.listData);
+    }
+  },
+}
+</script>
+<style scoped>
+  @import '../../static/css/index-list.css';
+</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
--- src/page/register.vue
+++ src/page/register.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="register">
-    <input type="text" placeholder="请输入名字" name="name" v-model="registerData.name"/>
-    <input type="text" placeholder="请输入年龄" name="age" v-model="registerData.age"/>
+    <inputView :listData="inputMsg"></inputView>
     <button class="registerBtn" @click="registerFun()">注册</button>
   </div>
 </template>
 
 
 <script>
+  import inputView from '../components/inputView'
 export default {
   data () {
     return {
@@ -16,14 +16,60 @@ export default {
         name:'',
         age:'',
       },
+      inputMsg:[
+        {
+          textTitle:'用户名',
+          inputType:'true',
+          inputAttr:{
+            inputClasses:'aaa ddd',
+            type:'text',
+//          disabled:'',
+            name:'name',
+//          readonly:'',
+            placeholder:'请输入名字',
+//            widthChange:'200px',
+            val:'name',
+          },
+        },
+        {
+          textTitle:'年龄',
+          listType:'true',
+          inputAttr:{
+            cType:'text',
+            inputClasses:'bbb',
+            type:'number',
+//          disabled:'',
+            name:'name1',
+            placeholder:'请输入年龄',
+            widthChange:'200px',
+//          readonly:'',
+            val:56
+          },
+        },
+//        {
+//          cType:'text',
+//          inputClasses:'bbb',
+//          type:'number',
+////          disabled:'',
+//          name:'name1',
+//          placeholder:'请输入年龄',
+//          widthChange:'200px',
+////          readonly:'',
+//          val:56
+//        },
+
+      ]
     }
+  },
+  components:{
+    inputView
   },
   methods : {
     registerFun(){
       console.log(this.registerData);
-
     },
-  }
+  },
+
 }
 </script>
 
